3e070028adf4fba8436d235908ede09ada1f3544
SHERINE-6 <143429583+SHERINE-6@users.noreply.github.com>
2023-08-28 21:55:14 +0530

    Deleted the Header & Caption

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,5 @@
 import streamlit;
 streamlit.title('My Parents New Healthy Dinner')
-"""
-streamlit.header('Breakfast Menu')
-streamlit.caption('Omega 3 & Blueberry Oatmeal')
-streamlit.caption('Kale, Spinach & Rocket Smoothie')
-streamlit.caption('Hard-Boiled Free-Range Eggs')
-"""
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
